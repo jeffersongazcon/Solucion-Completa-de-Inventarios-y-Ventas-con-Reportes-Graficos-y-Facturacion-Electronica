6fa78c91f016be4529ce0930350d95702c5d2374
jeffersongazcon <143918762+jeffersongazcon@users.noreply.github.com>
2024-04-04 16:58:46 -0500

    -_-

--- proyecto/Program.cs
+++ proyecto/Program.cs
@@ -14,6 +14,7 @@ namespace proyecto
         [STAThread]
         static void Main()
         {
+            Console.Write("el gato negro");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
