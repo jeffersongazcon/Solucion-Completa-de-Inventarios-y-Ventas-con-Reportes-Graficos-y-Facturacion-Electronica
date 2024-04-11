7948061c8231633763aee19523ceecc514a0e900
UPCJOSE <143562627+UPCJOSE@users.noreply.github.com>
2024-04-11 18:22:00 -0500

    Lopez

---
+++ Entity(Entidad)/Permisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Entidad_
+{
+    public class Permisos
+    {
+        public int AfinidadPermiso { get; set; }
+        public string TipoDeMenu { get; set; }
+        public string FechaRegistro { get; set; }
+        public Cargo TipoDeCargo { get; set; }
+    }
+}
---
+++ Entity(Entidad)/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_Entidad_
+{
+    public class Venta
+    {
+        public string AfiliacionVenta{ get; set; }
+        public Usuario Usuario { get; set; }
+        public string Factura { get; set; }
+        public string DocumentoCliente { get; set; }
+        public string NombreCliente{ get; set; }
+        public decimal MontoPago{get; set; }
+        public decimal MontoCambio {  get; set; }
+        public decimal MontoTotal { get; set; }
+        public string FechaRegistro { get; set; }
+        public List<DetalleVenta> DetalleVentas { get; set; }
+    }
+}
