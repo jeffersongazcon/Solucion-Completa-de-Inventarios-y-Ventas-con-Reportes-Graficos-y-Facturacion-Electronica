20e9e2bbd84800f520a84549702a45342396c21d
1Daniel-cogollo <dandrescogollo@unicesar.edu.co>
2024-04-12 14:01:23 -0500

    Icrud añadidos en capa de datos, falta validacion y logica

---
+++ DAL(Datos)/IcrudCategoria.cs
@@ -0,0 +1,17 @@
+﻿using Entity_Entidad_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Datos_
+{
+    public interface IcrudCategoria
+    {
+        string Add(Categoria categoria);
+        string Update(Categoria categoria);
+        string Delete(Categoria categoria);
+        List<Categoria> GetAll();
+    }
+}
---
+++ DAL(Datos)/IcrudCompra.cs
@@ -0,0 +1,17 @@
+﻿using Entity_Entidad_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Datos_
+{
+    public interface IcrudCompra
+    {
+        string Add(Compra compra);
+        string Update(Compra compra);
+        string Delete(Compra compra);
+        List<Compra> GetAll();
+    }
+}
