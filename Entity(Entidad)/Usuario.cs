6b2eb511594d81e5981400be0ceb78e9a4355861
jeffersongazcon <143918762+jeffersongazcon@users.noreply.github.com>
2024-04-24 13:02:03 -0500

    modificaciones a los permisos de usuario con la base de datos

--- Entity(Entidad)/Usuario.cs
+++ Entity(Entidad)/Usuario.cs
@@ -8,13 +8,9 @@ namespace Entity_Entidad_
 {
     public class Usuario
     {
-        public int AfinidadUsuario { get; set; }
-        public string Documento { get; set; }
-        public string Nombre {get; set;}
-        public string Clave { get; set;}
-        public string Telefono { get; set; }
-        public bool Estado { get; set; }
-        public string FechaRegistro { get; set; }
-        public Cargo IdCargo { get; set; }
+
+        public static int IdUser { get; set; }
+        public static int FirsName { get; set; }
+
     }
 }
